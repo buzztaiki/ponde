125f93537971254cf199130d3703daa9994559ce
Taiki Sugawara <buzz.taiki@gmail.com>
2025-11-30 20:27:53 +0900

    s/Key/KeyCode/

--- src/sink_device.rs
+++ src/sink_device.rs
@@ -1,5 +1,5 @@
 use evdev::uinput::{VirtualDevice, VirtualDeviceBuilder};
-use evdev::{AttributeSet, Key, RelativeAxisCode};
+use evdev::{AttributeSet, KeyCode, RelativeAxisCode};
 
 use crate::errors::Error;
 use crate::sink_event::SinkEvent;
@@ -11,10 +11,10 @@ pub struct SinkDevice {
 
 impl SinkDevice {
     pub fn create(name: &str) -> Result<Self, Error> {
-        let mut keys = AttributeSet::<Key>::new();
+        let mut keys = AttributeSet::<KeyCode>::new();
         // Note: when keyboard keys are enabled, it is not detected as a mouse
-        for code in Key::BTN_0.code()..=Key::BTN_THUMBR.code() {
-            keys.insert(Key::new(code));
+        for code in KeyCode::BTN_0.code()..=KeyCode::BTN_THUMBR.code() {
+            keys.insert(KeyCode::new(code));
         }
 
         let mut rel_axes = AttributeSet::<RelativeAxisCode>::new();
